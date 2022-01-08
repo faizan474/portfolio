b70e324299dc3bb5ebac3a11b1505026eb8539a4
Mohd Faizan <mdfaiz12343@gmail.com>
2022-01-08 18:45:57 +0530

    refactor socialContacts

--- src/components/common/social-contact/socialContact.js
+++ src/components/common/social-contact/socialContact.js
@@ -4,12 +4,13 @@ import './socialContact.css';
 
 function SocialContact() {
     const  data=SocialData;
+    data.sort((a,b)=>a.id-b.id);
     return (
         <div className="socialContact">
             {
                 data.map((profile)=>{
                     return (
-                        <a href={profile.link} key={profile.id.toString()}>
+                        <a style={{display: "table-cell"}} href={profile.link} key={profile.id.toString()} target="_blank">
                             <div className="social-icon-div">
                                 <img src={profile.icon} alt={profile.plateform} className="social-icon"></img>
 
